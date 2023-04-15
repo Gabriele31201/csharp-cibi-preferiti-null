63d5213400715d84d01fbc165ab03cc5051e565e
Gabriele31201 <gabri31201@gmail.com>
2023-04-15 17:12:08 +0200

    add array

---
+++ csharp-cibi-preferiti/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Create un progetto App Console CibiPreferiti con Visual Studio.
+Nel programma inizializzate un array con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi). 
+L’array deve essere già inizializzato nel programma ordinato già con i cibi dal più preferito al meno preferito, 
+e i vostri cibi preferiti non vanno chiesti all’utente tramite input (hard-coded).
+
+ 
+ 
+ Una volta dichiarato l’array di cibi preferiti, stampate a video le seguenti informazioni:
+        La lunghezza della classifica
+        La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
+        Il vostro cibo top (prima posizione della classifica)
+        Il vostro cibo preferito ma non troppo (ultima posizione della classifica
+ 
+ */
+
+
+
+string[] favoriteFood = {"pizza", "carne", "pasta", "cioccolato", "sushi" };
+
+
+
